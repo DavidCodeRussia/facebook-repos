a89e83ab5bada26ef3a8be5b4949539729dff55a
DavidCodeRussia <93557790+DavidCodeRussia@users.noreply.github.com>
2023-07-26 15:52:27 +0400

    project done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import * as svg from './assets/svg';
 import './App.css';
 
+type Repo = {
+  name: string;
+  stargazers_count: number;
+  forks_count: number;
+};
+
 function App() {
+  const [repos, setRepos] = useState<Repo | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    let getRepos = async () => {
+      setLoading(true);
+      let res = await fetch('https://api.github.com/users/facebook/repos');
+      if (res.ok) {
+        let json = await res.json();
+        setRepos(json);
+        setLoading(false);
+      } else {
+        alert('Ошибка HTTP: ' + res.status);
+        setLoading(false);
+      }
+    };
+    getRepos();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">Repositories facebook</div>
+      {loading ? (
+        <div className="App-inner">
+          <div>loading...</div>
+        </div>
+      ) : (
+        <div className="App-inner">
+          {Array.isArray(repos) &&
+            repos.map((item, key) => {
+              return (
+                <div className="App-repo" key={key}>
+                  <div className="first-part">
+                    <div className="serial-number">{key}.</div>
+                    <div className="name">{item.name}</div>
+                  </div>
+                  <div className="second-part">
+                    <div className="stars">
+                      <svg.Star />
+                      {item.stargazers_count}
+                    </div>
+                    <div className="forks-wrapper">
+                      <div className="forks-header">forks:</div>
+                      <div>{item.forks_count}</div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      )}
     </div>
   );
 }
